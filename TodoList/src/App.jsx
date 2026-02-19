dbb26cae1de40cc2d5d22907d71dc156baaaa5b3
kbm1353 <kbm1353@naver.com>
2026-02-19 17:11:06 +0900

    useReducer, useCallbackを使用してコンポーネントを最適化

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useReducer, useCallback } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
@@ -26,37 +26,91 @@ const mockData = [
   },
 ];
 
+// reducer: 実際に状態をどのように変更するかを定義した関数。dispatch が呼び出されると、React がこの関数を実行する。
+function reducer(state, action) {
+  // state: 現在の todos の値
+  // action: dispatch から送られたオブジェクト
+
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item,
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  // const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  // useReducer → 状態管理ロジックをコンポーネントの外部に分離するときに使う
+  // todos: 現在の状態（データ）の値
+  // dispatch: 状態を変更してほしいとリクエストする関数。この関数を実行するときに action オブジェクトを引数として渡す
+
   const idRef = useRef(3); // id値が変更されたときに不要なレンダリングを防ぐために、Refを使用
 
-  const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
+  // const onCreate = (content) => {
+  //   const newTodo = {
+  //     id: idRef.current++,
+  //     isDone: false,
+  //     content: content,
+  //     date: new Date().getTime(),
+  //   };
 
-    setTodos([newTodo, ...todos]);
-  };
+  //   setTodos([newTodo, ...todos]);
+  // };
 
-  const onUpdate = (targetId) => {
-    // todosステートの値の中で
-    // targetIdと一致するidを持つtodo itemのisDoneを変更する
+  // useCallback → 関数は最初の1回だけ作って、次からは作ったものをそのまま再利用する
+  // 1つ目の引数 = 実行を防ぎたい関数
+  // 2つ目の引数 = 依存配列、空であれば、この関数は絶対に再生成しないという意味
+  const onCreate = useCallback((content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  }, []);
 
-    // 引数：todos配列の中で、targetIdと一致するidを持つ要素のデータだけを変更した新しい配列
-    setTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo,
-      ),
-    );
-  };
+  // const onUpdate = (targetId) => {
+  //   // todosステートの値の中で
+  //   // targetIdと一致するidを持つtodo itemのisDoneを変更する
 
-  const onDelete = (targetId) => {
-    // 引数：Todos配列の中で、TargetIdと一致するidを持つ要素のみを削除した新しい配列
-    setTodos(todos.filter((todo) => todo.id !== targetId));
-  };
+  //   // 引数：todos配列の中で、targetIdと一致するidを持つ要素のデータだけを変更した新しい配列
+  //   setTodos(
+  //     todos.map((todo) =>
+  //       todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo,
+  //     ),
+  //   );
+  // };
+
+  const onUpdate = useCallback((targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  }, []);
+
+  // const onDelete = (targetId) => {
+  //   // 引数：Todos配列の中で、TargetIdと一致するidを持つ要素のみを削除した新しい配列
+  //   setTodos(todos.filter((todo) => todo.id !== targetId));
+  // };
+
+  const onDelete = useCallback((targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  }, []);
 
   return (
     <div className="App">
